171efaf9ef8f6bf276b7ec677e0a4d0f49a6c491
Hoorad <hourrad.f@gmail.com>
2023-10-02 20:14:09 +1300

    Added tablemode && made some changes to neorg

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -5,13 +5,14 @@ return {
     config = function()
         require("neorg").setup {
             load = {
-                ["core.defaults"] = {}, -- Loads default behaviour
-                ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                ["core.dirman"] = { -- Manages Neorg workspaces
+                ["core.defaults"] = {},                                -- Loads default behaviour
+                ["core.concealer"] = { config = { icon_preset = "diamond" } }, -- Adds pretty icons to your documents
+                ["core.dirman"] = {                                    -- Manages Neorg workspaces
                     config = {
                         workspaces = {
                             notes = "~/org/neorg/notes",
                         },
+                        default_workspace = notes,
                     },
                 },
             },
