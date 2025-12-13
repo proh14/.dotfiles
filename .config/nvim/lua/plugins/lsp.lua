439013a3fe25e2641923ce5efec2622f3f3f69d6
Hoorad Farrokh <hourrad.f@gmail.com>
2025-12-13 17:01:07 +1300

    deleted things I don't use

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,5 +3,14 @@ return {
   config = function()
     vim.lsp.enable "lua_ls"
     vim.lsp.enable "clangd"
+    vim.diagnostic.config({
+      virtual_text = {
+        true,
+      },
+      signs = true,
+      underline = true,
+      update_in_insert = false,
+      severity_sort = true,
+    })
   end,
 }
